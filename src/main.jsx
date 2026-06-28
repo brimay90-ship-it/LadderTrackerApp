efc1975d4bc2ef1cb880e17770508bbeab1af06b
brimay90-ship-it <brimay90-ship-it@users.noreply.github.com>
2026-06-28 13:26:23 -0700

    Make preview app self-contained and add local component copy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../../LadderTracker_1.jsx';
+import App from './LadderTracker_1.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
